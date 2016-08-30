035c5e0670aa41ec5a68700d49bb20cd4f0a880d
Stéphane Petit <stephane.petit95@gmail.com>
2016-08-30 23:43:33 +0200

    TODO list to implement basic eventsourcing without CQRS

--- meal-infra/src/main/java/repository/Command.java
+++ meal-infra/src/main/java/repository/Command.java
@@ -4,4 +4,8 @@ package repository;
  * Created by spetit on 11/08/2016.
  */
 public abstract class Command {
+
+    // decide :: Command -> State -> [Event]
+
+    // apply :: State -> Event -> State
 }
--- meal-infra/src/main/java/repository/MealYearlyScaleUpdated.java
+++ meal-infra/src/main/java/repository/MealYearlyScaleUpdated.java
@@ -49,4 +49,55 @@ public class MealYearlyScaleUpdated extends Event<MealYearlyScale, MealYearlySca
         EventRecord record = new EventRecord(666L, "aggId", "mealScale", 1L, 1L, 1L, ZonedDateTime.now(), event);
     }*/
 
+    //decide :: Command -> State -> [Event]
+
+    //apply :: State -> Event -> State
+
+    //event sourcing sqns cqrs
+    // j'appelle l'api
+    // je cree une commande a partir de l'api
+    // j'appelle le repo a partir de la commande
+    // le repo ramene l entite domaine a partir de la commande
+    // on cree les evenements associes a partir de la commande et de l'etat courant (decide)
+    // on cree ou on ramene l'agregat a partir de l'id de l'entite
+    // on ajoute les evenements a l'agregat
+    // on sauvegarde l agregat avec les nouveaux evenements
+
+    // event sourcing sans cqrs
+    // j appelle l'api
+    // je cree une query a partir de l'api
+
+    // je veux une entite specifique
+    // j'appelle le repo avec la qeury
+    // le repos ramene l'agregat avec l'id
+    // je construit l'entite ou la projection en rejouant tous les evenements (apply)
+    // je cree un DTO pour afficher
+
+    // je veux une liste d'entites filtrees
+    // j'appelle le repo avec la qeury
+    // le repos ramene les agregats avec le type des entites
+    // je construit les entites en rejouant tous les evenements pour chaque (apply)
+    // je filtre en java avec la query
+    // je convertis en DTO
+
+    // pb de performance
+    // objets et aggregats mal adaptes aux besoins de la requete/query
+    // il faut tout reconstruire a chaque requete
+    // besoin de dto pour afficher
+    // operations lourdes en synchrone
+
+
+
+    // event sourcing avec cqrs
+    // j appelle l'api
+    // je cree une query a partir de l'api
+    // toutes mes tables sont des projections cree specialement pour les besoins d'affichage
+    // j appelle le repo avec la commande
+    // je retourne le resultat
+
+
+    //TODO handlers events bus pour faire ca en async
+
+
+
 }
